bc0b1986da58a798effe9b0f19ac4e5b8525103d
offdnsh <offdnsh@gmail.com>
2021-05-11 09:43:35 +0300

    make it better 1

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const serverStatic = require('serve-static')
-const path = require('path')
 
 const app = express()
 
-app.use('/', serverStatic(path.join(__dirname, '/dist')))
+app.use(express.static(__dirname, '/dist/'))
+
+app.get(/.*/, function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html')
+})
 
 const port = process.env.PORT || 8080
 
