17fe7a57943c4312d8658533dd373c681ada0027
offdnsh <offdnsh@gmail.com>
2021-05-18 10:20:22 +0300

    make 34

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,12 @@ export default {
 </script>
 
 <style>
-* {
+body, h1, h2, h3, h4, h5, .ui {
     font-family: 'Nunito', 'Segoe UI', sans-serif !important;
     letter-spacing: .5px !important;
+}
+
+i.icon {
+    font-family: Icons;
 }
 </style>
