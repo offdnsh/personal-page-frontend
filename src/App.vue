94a471735af31622576790af3e2dee4eae5fd41f
offdnsh <offdnsh@gmail.com>
2021-05-18 10:23:41 +0300

    make 36

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,9 @@ body, h1, h2, h3, h4, h5, .ui {
 i.icon {
     font-family: Icons;
 }
+
+.ui.form input:not([type]), .ui.form input[type=date], .ui.form input[type=datetime-local], .ui.form input[type=email], .ui.form input[type=file], .ui.form input[type=number], .ui.form input[type=password], .ui.form input[type=search], .ui.form input[type=tel], .ui.form input[type=text], .ui.form input[type=time], .ui.form input[type=url] {
+    font-family: 'Nunito', 'Segoe UI', sans-serif !important;
+    letter-spacing: .5px !important;
+}
 </style>
